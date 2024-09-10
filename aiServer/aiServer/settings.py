aae4b9604ceaedcca3e3fc18043a8acd20002ebb
hl3470 <hl3470@naver.com>
2024-09-10 13:37:36 +0900

    refactor: mysql로 db 변경

--- aiServer/aiServer/settings.py
+++ aiServer/aiServer/settings.py
@@ -4,6 +4,8 @@ from pathlib import Path
 
 from django.core.exceptions import ImproperlyConfigured
 
+from aiServer import my_settings
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -80,12 +82,7 @@ WSGI_APPLICATION = 'aiServer.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = my_settings.DATABASES
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
